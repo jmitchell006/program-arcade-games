7c3ecca4d6560a90d59b6543dfea3e39cb069101
jmitchell006 <jamitchell@stcharlessd.org>
2017-11-02 10:12:57 -0500

    completed chapter 1 completed chapter 2

--- Lab 01 - Calculator/lab_01_part_a.py
+++ Lab 01 - Calculator/lab_01_part_a.py
@@ -1,1 +1,5 @@
-#
+#!/usr/bin/env python3
+
+Fehrenheit = int(input("Enter a temperature in Fahrenheit: "))
+Celsius = (Fehrenheit x .556)
+print(Celsius)
--- Lab 01 - Calculator/lab_01_part_b.py
+++ Lab 01 - Calculator/lab_01_part_b.py
@@ -1,1 +1,3 @@
-#
+chris = input('is chris gay')
+if chris == (yes):
+    print('YOurR RIGHT')
